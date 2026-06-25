85e577c240980f207deeb940221495d3f27978ca
shikharsharma613-png <shikharsharma613@gmail.com>
2026-06-25 17:01:46 +0530

    Update script.js

--- img/script.js
+++ img/script.js
@@ -56,7 +56,7 @@ function generateMessage(noCount) {
 }
 
 function changeImage(image) {
-  catImg.src = `img/cat-${image}.jpg`;
+  catImg.src = `cat-${image}.jpg`;
 }
 
 function updateNoButtonText() {
